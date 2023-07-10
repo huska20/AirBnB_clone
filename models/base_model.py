6ca219dfd9bcb6f4f190a2f21d301272921bd78e
Adesanya07 <gadesanya22@gmail.com>
2023-07-10 21:21:41 +0100

    testing

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This script is the basemodel"""
+
+import uuid
+from datetime import datetime
+
+
+class Basemodel():
+     """Class from which all other classes will inherit"""
+     
+
